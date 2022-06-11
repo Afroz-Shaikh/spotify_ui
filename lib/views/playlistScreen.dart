30180f26a0bc8ce3c66624766fd842ecb8dedca0
Afroz-Shaikh <afrozshaikh725@gmail.com>
2022-06-11 21:12:41 +0530

    added gradient screens

--- lib/views/playlistScreen.dart
+++ lib/views/playlistScreen.dart
@@ -14,14 +14,64 @@ class _PlayListScreenState extends State<PlayListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        elevation: 0,
+        leadingWidth: 140,
+        backgroundColor: Colors.transparent,
+        leading: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              InkWell(
+                  onTap: () {},
+                  customBorder: const CircleBorder(),
+                  child: Container(
+                    padding: EdgeInsets.all(6),
+                    decoration: BoxDecoration(
+                        color: Colors.black26, shape: BoxShape.circle),
+                    child: Icon(
+                      Icons.chevron_left,
+                      size: 28,
+                    ),
+                  )),
+              SizedBox(
+                width: 15,
+              ),
+              InkWell(
+                  onTap: () {},
+                  customBorder: const CircleBorder(),
+                  child: Container(
+                    padding: EdgeInsets.all(6),
+                    decoration: BoxDecoration(
+                        color: Colors.black26, shape: BoxShape.circle),
+                    child: Icon(
+                      Icons.chevron_right,
+                      size: 28,
+                    ),
+                  ))
+            ],
+          ),
+        ),
+      ),
       body: Container(
         width: double.infinity,
         decoration: BoxDecoration(
             gradient: LinearGradient(
                 begin: Alignment.topCenter,
                 end: Alignment.bottomCenter,
-                colors: [Colors.blue, Colors.green])),
+                colors: [
+              // Color(0xffE5DBBD),
+              Colors.blue,
+              Theme.of(context).backgroundColor
+            ],
+                stops: const [
+              0,
+              0.3
+            ])),
       ),
     );
   }
